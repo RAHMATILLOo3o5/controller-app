52c296db66359d516496815dd4650386d7c1aa6b
RAHMATILLOo3o5 <husanboyev2oo5@gmail.com>
2023-01-09 15:06:37 +0500

    edit

--- restapi/controllers/BaseController.php
+++ restapi/controllers/BaseController.php
@@ -16,7 +16,14 @@ class BaseController extends ActiveController
         $behaviors = parent::behaviors();
 
         $behaviors['corsFilter'] = [
-            'class' => Cors::class
+            'class' => Cors::class,
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 86400,
+            ],
         ];
 
         return $behaviors;
