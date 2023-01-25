438f32a3332c01247c45283c750f9dae4147c7f9
RAHMATILLOo3o5 <husanboyev2oo5@gmail.com>
2023-01-25 20:50:31 +0500

    new

--- restapi/modules/seller/controllers/SellingController.php
+++ restapi/modules/seller/controllers/SellingController.php
@@ -2,7 +2,31 @@
 
 namespace restapi\modules\seller\controllers;
 
-class SellingController extends \restapi\controllers\BaseController
+use common\models\Selling;
+use restapi\controllers\BaseController;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\MethodNotAllowedHttpException;
+
+class SellingController extends BaseController
 {
+    public $modelClass = Selling::class;
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['index']['prepareDataProvider'] = [$this, 'data'];
+
+        return $actions;
+    }
+
+    public function data()
+    {
+        $provider = new ActiveDataProvider([
+            'query' => Selling::find()->where(['worker_id' => Yii::$app->user->id])
+        ]);
+        return $provider;
+    }
 
 }
