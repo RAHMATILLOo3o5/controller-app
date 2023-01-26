a159ab06df57e4bdd6ad30a4a2dcf84e9081e077
RAHMATILLOo3o5 <husanboyev2oo5@gmail.com>
2023-01-26 21:36:02 +0500

    edited

--- restapi/controllers/StatisticsController.php
+++ restapi/controllers/StatisticsController.php
@@ -3,6 +3,8 @@
 namespace restapi\controllers;
 
 use common\models\Statistics;
+use Yii;
+use yii\data\ActiveDataProvider;
 
 class StatisticsController extends BaseController
 {
@@ -12,6 +14,24 @@ class StatisticsController extends BaseController
     {
         $actions = parent::actions();
         unset($actions['create'], $actions['update'], $actions['delete']);
+        $actions['index']['prepareDataProvider'] = [$this, 'data'];
         return $actions;
+    }
+
+    public function data()
+    {
+        $period = Yii::$app->request->get('period');
+        $data = new ActiveDataProvider([
+            'query' => Statistics::find()->where(['>=', 'period', date('Y-m-d H:i', strtotime("-{$period} days"))]),
+            'pagination' => [
+                'pageSize' => 50
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ],
+        ]);
+        return $data;
     }
 }
