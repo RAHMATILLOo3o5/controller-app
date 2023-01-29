82f8e3941c787df3372029114fc9ca4c0d63d179
RAHMATILLOo3o5 <husanboyev2oo5@gmail.com>
2023-01-29 14:23:55 +0500

    edited

--- restapi/modules/seller/controllers/SellingController.php
+++ restapi/modules/seller/controllers/SellingController.php
@@ -26,20 +26,28 @@ class SellingController extends BaseController
     public function actionSellingDebt()
     {
         $model = new Selling();
-
         $backlog = new Backlog();
-        if ($backlog->load(Yii::$app->request->post()) && $model->load(Yii::$app->request->post())) {
-            $model->type_pay = Selling::PAY_DEBT;
-            $model->save();
-            $backlog->selling_id = $model->id;
-            if ($backlog->save()) {
-                return $backlog->saved();
+        if (Yii::$app->request->isPost) {
+            if ($backlog->load(Yii::$app->request->post(), '') && $model->load(Yii::$app->request->post(), '')) {
+                $model->type_pay = Selling::PAY_DEBT;
+                $model->save();
+                $backlog->selling_id = $model->id;
+                if ($backlog->save()) {
+                    return $backlog->saved();
+                } else {
+                    return [
+                        $backlog->errors,
+                        $model->errors
+                    ];
+                }
             } else {
                 return [
                     $backlog->errors,
                     $model->errors
                 ];
             }
+        } else {
+            throw new MethodNotAllowedHttpException("Method Not Allowed. This URL can only handle the following request methods: POST.");
         }
     }
 
