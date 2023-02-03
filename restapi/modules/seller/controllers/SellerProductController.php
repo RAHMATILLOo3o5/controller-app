861a90e61012d2b4b4c4a39778dde3ed7c3cab04
RAHMATILLOo3o5 <husanboyev2oo5@gmail.com>
2023-02-03 15:54:56 +0500

    edit

--- restapi/modules/seller/controllers/SellerProductController.php
+++ restapi/modules/seller/controllers/SellerProductController.php
@@ -3,14 +3,24 @@
 namespace restapi\modules\seller\controllers;
 
 use restapi\controllers\BaseController;
+use restapi\controllers\ProductController;
 use restapi\models\ProductModel;
 use yii\rest\Serializer;
 
-class SellerProductController extends BaseController
+class SellerProductController extends ProductController
 {
     public $modelClass = ProductModel::class;
     
     public $serializer = [
         'class' => Serializer::class,
     ];
+
+    public function actions()
+    {
+        $action = parent::actions();
+        unset($action['create']);
+        unset($action['update']);
+        return $action;
+    }
+
 }
