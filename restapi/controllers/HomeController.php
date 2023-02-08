50aa1b33354d9f0dd25dc3f0219800c26bf97d41
RAHMATILLOo3o5 <husanboyev2oo5@gmail.com>
2023-02-08 11:37:50 +0500

    edit

--- restapi/controllers/HomeController.php
+++ restapi/controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace restapi\controllers;
 use common\models\Selling;
 use yii\rest\Controller;
 use common\models\Statistics;
+use component\GetCurrency;
 use restapi\models\CategoryModel;
 use restapi\models\ProductModel;
 use restapi\models\SellingModel;
@@ -34,6 +35,11 @@ class HomeController extends Controller
         $data['sell_product_amount'] = [
             'unit_kg' => $selling_query->where(['product_category.unit' => CategoryModel::UNIT_KG])->sum('sell_amount') ?? 0,
             'unit_each' => $selling_query->where(['product_category.unit' => CategoryModel::UNIT_EACH])->sum('sell_amount') ?? 0
+        ];
+        $data['currency_value'] = [
+            'usd' => GetCurrency::getData(date('Y-m-d')),
+            'eu' => GetCurrency::getData(date('Y-m-d'), 'EUR'),
+            'rub' => GetCurrency::getData(date('Y-m-d'), 'RUB')
         ];
         $data['last_month_stat'] = Statistics::find()->where(['>=', 'period', date('Y-m-d H:i', strtotime("-30 days"))])->all();
 
